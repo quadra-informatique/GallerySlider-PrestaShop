030859d9495906e80bcee5f016e3f16e91762db7
Saraspady Appasamy <saraspady.appasamy@quadra-informatique.fr>
2012-01-05 09:05:21 +0000

    traductions

---
+++ fr.php
@@ -0,0 +1,21 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_edf02b3384f8fd2e0d5d408b7878e9c6'] = 'Carousel de gallerie d\'images';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_bf7882af2efa7960b51180680b434247'] = 'Affiche un carousel des images chargées sur la page d\'accueil';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_8096e89c4a1d3e3036fcfe6cb5d0d0c6'] = 'L\'image ne peut etre effacer';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_3832f6deff5d50413adcbc9c06388c32'] = 'Type de fichier invalide';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_465ac27fb7e011be23f072a42d13524a'] = 'Une erreur s\'est produite lors du chargement de l\'image';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_5a745e9e018bcf349109ab2ae05c14d2'] = 'Une erreur s\'est produite lors de la génération des images';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_cbc3c52fad9eade06d83220c0d58f3c0'] = 'Une erreur s\'est produite lors de la génération des images';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_2aa3aa9d021c7cfffb5afa08f52fbc51'] = 'Hauteur';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_6fed80a8c8ded2f5e14a687e4a443abc'] = 'Largeur';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_76639027bb72d9ad84ba46b7494f8ff0'] = 'Générer les images';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_b9ff25ed23c2f2f00933af209883429c'] = 'Affichage vertical';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_ec211f7c20af43e742bf2570c3cb84f9'] = 'Ajouter';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_be53a0541a6d36f6ecb879fa2c584b08'] = 'Image';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_705650e9ab808035ee18d703f971c3f2'] = 'Titre';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_97e7c9a7d06eac006a28bf05467fcc8b'] = 'Lien';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_f2a6c498fb90ee345d997f888fce3b18'] = 'Effacer';
+$_MODULE['<{quadragalleryslider}prestashop>quadragalleryslider_c9cc8cce247e49bae79f15173ce97354'] = 'Sauvegarder';
